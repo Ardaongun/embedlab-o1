1a1c0584aee1e7a25ac79975aac12b4dd60ed3c1
Arda Ongun <ardaongun3@gmail.com>
2025-12-14 04:16:36 +0300

    Add Logtail integration and enhance logger configuration for improved logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -15,17 +15,41 @@ const logger = pino({
     remove: true,
   },
 
-  transport:
-    process.env.NODE_ENV !== "prod"
-      ? {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            translateTime: "SYS:standard",
-            ignore: "pid,hostname",
-          },
-        }
-      : undefined,
+  transport: {
+    targets: [
+      ...(process.env.NODE_ENV === "development"
+        ? [
+            {
+              target: "pino-pretty",
+              options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+                ignore: "pid,hostname",
+              },
+            },
+          ]
+        : []),
+
+      ...(process.env.LOGTAIL_SOURCE_TOKEN
+        ? [
+            {
+              target: "@logtail/pino",
+              options: {
+                sourceToken: process.env.LOGTAIL_SOURCE_TOKEN,
+                options: {
+                  endpoint: process.env.LOGTAIL_URL,
+                },
+              },
+            },
+          ]
+        : []),
+
+      ...(process.env.NODE_ENV !== "development" &&
+      !process.env.LOGTAIL_SOURCE_TOKEN
+        ? [{ target: "pino/file", options: { destination: 1 } }] // 1 = stdout
+        : []),
+    ],
+  },
 });
 
 export default logger;
